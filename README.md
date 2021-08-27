d9460be8b322d21220147f15b2428838f8c54404
DarkDinDoN <maxime.chicault@gmail.com>
2021-08-27 21:01:56 +0200

    First release

---
+++ README.md
@@ -0,0 +1,5 @@
+![](https://img.shields.io/badge/Foundry-v0.8.9-informational)
+![Latest Release Download Count](https://img.shields.io/github/downloads/DarKDinDoN/party-sheet/latest/module.zip)
+
+# Party Sheet
+A small foundry module to replace the player hud with a more convenient interface.
---
+++ modules/settings/settings.js
@@ -0,0 +1,23 @@
+import constants from './constants.js'
+
+const partySheetDefault = { top: 500, left: 20 }
+
+export default function registerSettings() {
+    game.settings.register(constants.moduleName, 'party-sheet-box-position', {
+        scope: 'client',
+        config: false,
+        default: partySheetDefault
+    })
+
+    game.settings.register(constants.moduleName, 'party-sheet-hide-default-players', {
+        name: 'Hide the default players HUD',
+        hint: 'Enable this to hide the default players HUD (bottom left side of the screen).',
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => {
+            window.location.reload()
+        }
+    })
+}
